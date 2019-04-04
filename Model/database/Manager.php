4795b019057f6a2c39719fc6e26e38ea7a613541
sey_9 <sey_91360@hotmail.fr>
2019-04-04 22:14:54 +0200

    Creation of the class post , edit of the request to DB to return a Object

--- Model/database/Manager.php
+++ Model/database/Manager.php
@@ -4,22 +4,14 @@ namespace App\Model\Database;
 use PDO;
 
 class Manager {
-    private $db_name;
-    private $db_user;
-    private $db_pass;
-    private $db_host;
+
     private $pdo;
 
-    public function __construct($db_name = 'p4_blog' , $db_user = 'root', $db_pass = '', $db_host = 'localhost') {
-        $this->db_name = $db_name;
-        $this->db_user = $db_user;
-        $this->db_pass = $db_pass;
-        $this->db_host = $db_host;
-    }
+
 
     public function getDB(){
         if ($this->pdo === null){
-            $pdo = new PDO("mysql:host=$this->db_host;dbname=$this->db_name;charset=utf8", "$this->db_user", "$this->db_pass");
+            $pdo = new PDO("mysql:host=localhost;dbname=p4_blog;charset=utf8", "root", "");
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo = $pdo;
         }
